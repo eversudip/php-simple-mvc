ac66da5c2827451e2dce1ce4dbb880b457cd5261
sudip <sudip@sudips-MacBook-Pro.local>
2016-10-11 12:39:18 +0545

    separate view file with tpl.php

--- index.php
+++ index.php
@@ -1,34 +1,4 @@
-<!doctype html>
-<html class="no-js" lang="">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>mvc php</title>
-    <meta name="description" content="${2}">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+$greetings = 'Namaste';
 
-
-    <link rel="apple-touch-icon" href="apple-touch-icon.png">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
-
-</head>
-<body>
-
-
-    <?php $greetings = 'Namaste'; ?>
-
-    <p>Hello world! <?= $greetings; ?></p>
-
-    <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.4.min.js"><\/script>')</script>
-
-
-    <script>
-        window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
-        ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
-    </script>
-    <script src="https://www.google-analytics.com/analytics.js" async defer></script>
-</body>
-</html>
+require ('index.tpl.php');
