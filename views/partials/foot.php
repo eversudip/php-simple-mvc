1ecb9192db5902e1078e63dddf0c59b2e7a5c427
sudipsubedi <sudipsubedi.np@gmail.com>
2016-10-12 09:59:29 +0545

    refactoring the views pages

---
+++ views/partials/foot.php
@@ -0,0 +1,11 @@
+<script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.4.min.js"><\/script>')</script>
+
+
+    <script>
+        window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+        ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+    </script>
+    <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+</body>
+</html>
