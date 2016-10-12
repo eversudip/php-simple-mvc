1bd87f4bc3646d0ab113d5a8890b5e97491c4bbe
sudipsubedi <sudipsubedi.np@gmail.com>
2016-10-12 09:49:52 +0545

    introducing app array

--- core/appbootstrap.php
+++ core/appbootstrap.php
@@ -1,10 +1,13 @@
 <?php
 
-$config = require('database.php');
+$app=[];
+
+$app['config'] = require('database.php');
+
 require('core/Router.php');
 require('core/Request.php');
 require('database/Connector.php');
 require('database/QueryBuilder.php');
 
 //return query builder instance
-return new QueryBuilder(Connector::make($config['database']));
+$app['database'] = new QueryBuilder(Connector::make($app['config']['database']));
