9714f4e7ecdaadec9db225e3b5a87b98bfe62083
sudipsubedi <sudipsubedi.np@gmail.com>
2016-10-12 14:58:49 +0545

    refactoring the controller logic

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -10,6 +10,7 @@ return array(
     'ComposerAutoloaderInit5704828c3e95c2fc3e120ecd1e14da96' => $vendorDir . '/composer/autoload_real.php',
     'Composer\\Autoload\\ClassLoader' => $vendorDir . '/composer/ClassLoader.php',
     'Connector' => $baseDir . '/core/database/Connector.php',
+    'PagesController' => $baseDir . '/controllers/PagesController.php',
     'QueryBuilder' => $baseDir . '/core/database/QueryBuilder.php',
     'Request' => $baseDir . '/core/Request.php',
     'Router' => $baseDir . '/core/Router.php',
