24ecef0dc7339af21283492c1dba75e3cd28880f
sudipsubedi <sudipsubedi.np@gmail.com>
2016-10-12 09:34:21 +0545

    docblocks in router

--- core/Router.php
+++ core/Router.php
@@ -4,19 +4,27 @@ class Router
 {
 	protected $routes = [];
 
+	/**
+	 * loading the routes 
+	 */
 	public static function load($file)
 	{
 		$router = new static;
 		require $file;
-
 		return $router;
 	}
+
+	/**
+	 * defining the routes
+	 */
 	public function define($routes)
 	{
-
 		$this->routes = $routes;
 	}
 
+	/**
+	 * directing the routes to the controller
+	 */
 	public function direct($uri)
 	{
 		if(array_key_exists($uri, $this->routes))
