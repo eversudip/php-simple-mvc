c4af1ff8b0dff9d87ed0497c37ed2c2f8641dfed
sudipsubedi <sudipsubedi.np@gmail.com>
2016-10-12 15:16:36 +0545

    some method added

--- appbootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$config = require('core/database.php');
-require('core/database/Connector.php');
-require('core/database/QueryBuilder.php');
-
-//return query builder instance
-return new QueryBuilder(Connector::make($config['database']));
